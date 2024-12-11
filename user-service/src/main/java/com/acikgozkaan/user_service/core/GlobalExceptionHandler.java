998bfb1e362fcc50d9b22076d5f93c211eaa72ea
Kaan Acikgoz <kaanacikgoz@outlook.com>
2024-12-12 01:36:31 +0300

    Add custom exception and global exception handler

--- user-service/src/main/java/com/acikgozkaan/user_service/core/GlobalExceptionHandler.java
+++ user-service/src/main/java/com/acikgozkaan/user_service/core/GlobalExceptionHandler.java
@@ -1,4 +1,17 @@
-package com.acikgozkaan.user_service.core.exception;
+package com.acikgozkaan.user_service.core;
 
+import com.acikgozkaan.user_service.core.exception.UserNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<String> handleUserNotFoundException(UserNotFoundException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+
 }
