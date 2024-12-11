83122aba66d0776ec6157b909de8b8023c82508c
Kaan Acikgoz <kaanacikgoz@outlook.com>
2024-12-12 01:33:37 +0300

    Add User repository

--- user-service/src/main/java/com/acikgozkaan/user_service/repository/UserRepository.java
+++ user-service/src/main/java/com/acikgozkaan/user_service/repository/UserRepository.java
@@ -1,4 +1,9 @@
 package com.acikgozkaan.user_service.repository;
 
-public interface UserRepository {
+import com.acikgozkaan.user_service.entity.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
 }
