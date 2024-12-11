29590c9909b909f5b5df8bf13f62f33ef20828fa
Kaan Acikgoz <kaanacikgoz@outlook.com>
2024-12-12 01:32:26 +0300

    Add User entity

--- user-service/src/main/java/com/acikgozkaan/user_service/entity/User.java
+++ user-service/src/main/java/com/acikgozkaan/user_service/entity/User.java
@@ -1,4 +1,31 @@
 package com.acikgozkaan.user_service.entity;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Document(collection = "user")
 public class User {
+
+    @Id
+    private String id;
+
+    @NotBlank(message = "Name cannot be empty")
+    private String name;
+
+    @NotBlank(message = "Surname cannot be empty")
+    private String surname;
+
+    @Email(message = "Email must be valid")
+    @Indexed(unique = true)
+    private String email;
+
 }
