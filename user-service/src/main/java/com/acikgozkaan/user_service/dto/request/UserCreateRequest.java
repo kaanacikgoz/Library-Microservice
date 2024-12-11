eb36c072944fd42c3deba4f4d29e58aa117dd87c
Kaan Acikgoz <kaanacikgoz@outlook.com>
2024-12-12 01:35:51 +0300

    Add dto for create and update requests

--- user-service/src/main/java/com/acikgozkaan/user_service/dto/request/UserCreateRequest.java
+++ user-service/src/main/java/com/acikgozkaan/user_service/dto/request/UserCreateRequest.java
@@ -1,4 +1,29 @@
 package com.acikgozkaan.user_service.dto.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserCreateRequest {
+
+    @NotBlank(message = "Enter valid name")
+    @Size(max = 50, message = "Name must be less than 50 characters")
+    private String name;
+
+    @NotBlank(message = "Enter valid surname")
+    @Size(max = 50, message = "Surname must be less than 50 characters")
+    private String surname;
+
+    @Email(message = "Enter valid email")
+    @Size(max = 100, message = "Email must be less than 100 characters")
+    private String email;
+
 }
