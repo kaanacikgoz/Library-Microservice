d920a7089bad0d39818ec267ab30210a08201bbb
Kaan Acikgoz <kaanacikgoz@outlook.com>
2024-12-12 03:25:48 +0300

    Add category enum

--- book-service/src/main/java/com/acikgozkaan/book_service/entity/Category.java
+++ book-service/src/main/java/com/acikgozkaan/book_service/entity/Category.java
@@ -1,4 +1,15 @@
 package com.acikgozkaan.book_service.entity;
 
 public enum Category {
+    HORROR, MYSTERY,  FANTASY, ROMANCE, DRAMA, THRILLER, FICTION, ADVENTURE, HISTORY, BIOGRAPHY;
+
+    public static Category fromString(String name) {
+        for (Category category : Category.values()) {
+            if (category.name().equalsIgnoreCase(name)) {
+                return category;
+            }
+        }
+        throw new IllegalArgumentException("Unknown category: " + name);
+    }
+
 }
