07f620bb55e1c453a36a1801d2c3f7995b8ffed6
Kaan Acikgoz <kaanacikgoz@outlook.com>
2024-12-12 14:26:09 +0300

    Add borrowing repository

--- borrowing-service/src/main/java/com/acikgozkaan/borrowing_service/repository/BorrowingRepository.java
+++ borrowing-service/src/main/java/com/acikgozkaan/borrowing_service/repository/BorrowingRepository.java
@@ -1,4 +1,9 @@
 package com.acikgozkaan.borrowing_service.repository;
 
-public class BorrowingRepository {
+import com.acikgozkaan.borrowing_service.entity.Borrowing;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BorrowingRepository extends JpaRepository<Borrowing, Long> {
 }
