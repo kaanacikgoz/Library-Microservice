c322f05f54fb09a88ac02f5070734c1e7d13dc3b
Kaan Acikgoz <kaanacikgoz@outlook.com>
2024-12-12 03:30:34 +0300

    Add dto for save and update requests

--- book-service/src/main/java/com/acikgozkaan/book_service/dto/BookUpdateRequest.java
+++ book-service/src/main/java/com/acikgozkaan/book_service/dto/BookUpdateRequest.java
@@ -1,4 +1,34 @@
 package com.acikgozkaan.book_service.dto;
 
+import com.acikgozkaan.book_service.entity.Category;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class BookUpdateRequest {
+
+    @NotBlank(message = "Title cannot be empty")
+    @Size(max = 100, message = "Title must be less than 100 characters")
+    private String title;
+
+    @NotBlank(message = "Author cannot be empty")
+    @Size(max = 50, message = "Author must be less than 50 characters")
+    private String author;
+
+    @Enumerated(EnumType.STRING)
+    private Category category;
+
+    @Min(0)
+    private Integer stock;
+
 }
