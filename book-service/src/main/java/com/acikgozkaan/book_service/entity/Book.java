b460e5e93000752a0609dbfa4720731396f0b5e4
Kaan Acikgoz <kaanacikgoz@outlook.com>
2024-12-12 03:24:16 +0300

    Add book entity

--- book-service/src/main/java/com/acikgozkaan/book_service/entity/Book.java
+++ book-service/src/main/java/com/acikgozkaan/book_service/entity/Book.java
@@ -1,4 +1,28 @@
 package com.acikgozkaan.book_service.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String title;
+
+    private String author;
+
+    @Enumerated(EnumType.STRING)
+    private Category category;
+
+    private Integer stock;
+
 }
