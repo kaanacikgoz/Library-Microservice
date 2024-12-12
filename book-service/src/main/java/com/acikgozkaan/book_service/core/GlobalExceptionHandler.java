f4e39e73627e4e56cd0ffcb3fe6957d59e16203e
Kaan Acikgoz <kaanacikgoz@outlook.com>
2024-12-12 03:31:21 +0300

    Add custom exception and global exception handler

--- book-service/src/main/java/com/acikgozkaan/book_service/core/GlobalExceptionHandler.java
+++ book-service/src/main/java/com/acikgozkaan/book_service/core/GlobalExceptionHandler.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-public class GlobalHandlerException {
+public class GlobalExceptionHandler {
 
     @ExceptionHandler(BookNotFoundException.class)
     public ResponseEntity<String> handleBookNotFoundException(BookNotFoundException ex) {
--- book-service/src/main/java/com/acikgozkaan/book_service/core/exception/BookNotFoundException.java
+++ book-service/src/main/java/com/acikgozkaan/book_service/core/exception/BookNotFoundException.java
@@ -1,4 +1,8 @@
 package com.acikgozkaan.book_service.core.exception;
 
-public class BookNotFoundException {
+public class BookNotFoundException extends RuntimeException {
+
+    public BookNotFoundException(String message) {
+        super(message);
+    }
 }
