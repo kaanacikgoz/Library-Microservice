27f5714116161a536cb66e6b014dba79557dbda8
Kaan Acikgoz <kaanacikgoz@outlook.com>
2024-12-13 03:33:40 +0300

    Add handle custom exception

--- borrowing-service/src/main/java/com/acikgozkaan/borrowing_service/core/GlobalExceptionHandler.java
+++ borrowing-service/src/main/java/com/acikgozkaan/borrowing_service/core/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.acikgozkaan.borrowing_service.core;
 
+import com.acikgozkaan.book_service.core.exception.BookNotFoundException;
 import com.acikgozkaan.borrowing_service.core.exception.BorrowingNotFoundException;
+import com.acikgozkaan.user_service.core.exception.UserNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -11,6 +13,16 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(BorrowingNotFoundException.class)
     public ResponseEntity<String> handleBorrowingNotFoundException(BorrowingNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<String> handleUserNotFoundException(UserNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(BookNotFoundException.class)
+    public ResponseEntity<String> handleBookNotFoundException(BookNotFoundException ex) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
     }
 
